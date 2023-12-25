70af7d345a64a790c12ea1cc5463c1c70f97e142
Kayetic <bartek.bak@protonmail.com>
2023-12-25 12:31:09 +0100

    updated from new function in AWS, added eventID field

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,18 +2,18 @@ import boto3
 import json
 import logging
 from botocore.exceptions import ClientError
+import uuid
 
-# Set up logging
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+
 
 s3_client = boto3.client('s3')
-bucket_name = 'timetreebucket'
+bucket_name = 'shared-calendar-bucket'
 
 def lambda_handler(event, context):
-    version = event.get('version', '1.0')  # Default to 1.0 if version key is not present
-    logger.info('Payload version: ' + version)
-
+    version = event.get('version', '1.0')  # Default to '1.0' if version key is not present
+    
     # Determine HTTP method based on payload version
     if version == '2.0':
         # Handle version 2.0 specific logic
@@ -21,18 +21,16 @@ def lambda_handler(event, context):
     else:
         # Handle version 1.0 specific logic
         http_method = event.get('httpMethod')
-
-    logger.info('HTTP Method: ' + str(http_method))
-
-    # Additional log to confirm the event structure
-    logger.info('Received event: ' + json.dumps(event))
-
+    
     if http_method == 'GET':
         logger.info('Handling GET request')
         return handle_get_request(event)
     elif http_method == 'POST':
         logger.info('Handling POST request')
         return handle_post_request(event)
+    elif http_method == 'DELETE':
+        logger.info('Handling DELETE request')
+        return handle_delete_request(event)
     else:
         logger.warning('Received unexpected HTTP method')
         return {
@@ -40,91 +38,69 @@ def lambda_handler(event, context):
             'headers': get_cors_headers(),
             'body': json.dumps({'message': 'Method Not Allowed', 'method': http_method})
         }
-
-
-
-
+    
 
 def handle_get_request(event):
-    year = event['queryStringParameters']['year']
-    month = event['queryStringParameters']['month']
-    object_name = f'events/{year}/events-{month}.json'
-
     try:
-        data = s3_client.get_object(Bucket=bucket_name, Key=object_name)
-        file_content = data['Body'].read().decode('utf-8')
+        # Extract year and month from the query parameters
+        year = event['queryStringParameters']['year']
+        month = event['queryStringParameters']['month']
+        prefix = f'{year}/{month}/'
+        logger.info(f'Searching for events in {prefix}')
+
+        # List objects in the S3 bucket for the given year and month
+        response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+        events = []
+
+        for item in response.get('Contents', []):
+            event_object = s3_client.get_object(Bucket=bucket_name, Key=item['Key'])
+            event_content = event_object['Body'].read().decode('utf-8')
+            event_data = json.loads(event_content)
+            events.append(event_data)
+
+        logger.info(f'Retrieved {len(events)} events')
         return {
             'statusCode': 200,
-            'headers': get_cors_headers(),
-            'body': file_content
+            'body': json.dumps({'events': events})
         }
+
     except ClientError as e:
-        return handle_client_error(e)
+        logger.error('ClientError', exc_info=True)
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': 'Error fetching events', 'error': str(e)})
+        }
+    except Exception as e:
+        logger.error('Unhandled exception', exc_info=True)
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': 'An unexpected error occurred', 'error': str(e)})
+        }
 
 def handle_post_request(event):
+    # Extract year and month from the query parameters
     year = event['queryStringParameters']['year']
     month = event['queryStringParameters']['month']
-    new_event = json.loads(event['body'])
+    event_body = json.loads(event['body'])
     
-    object_name = f'events/{year}/events-{month}.json'
+    # Generate a unique identifier for the new event
+    event_id = str(uuid.uuid4())
+    
+    # Add the eventID to the event body
+    event_body['eventID'] = event_id
+    
+    # Construct the object key with the unique eventID
+    object_key = f'{year}/{month}/{event_id}.json'
 
+    # Save the new event to S3
     try:
-        # Try to get the existing file
-        data = s3_client.get_object(Bucket=bucket_name, Key=object_name)
-        existing_file_content = json.loads(data['Body'].read().decode('utf-8'))
-        existing_events = existing_file_content.get('events', [])
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'NoSuchKey':
-            existing_events = []
-        else:
-            return {
-                'statusCode': 500,
-                'headers': get_cors_headers(),
-                'body': json.dumps({'error': str(e)})
-            }
-
-    # Add the new event to the existing events
-    existing_events.append(new_event)
-
-    # Create the updated content in the desired format
-    updated_content = {
-        "events": existing_events
-    }
-
-    try:
-        # Write the updated content to the S3 object
-        s3_client.put_object(Bucket=bucket_name, Key=object_name, Body=json.dumps(updated_content))
+        s3_client.put_object(Bucket=bucket_name, Key=object_key, Body=json.dumps(event_body))
         return {
             'statusCode': 200,
-            'headers': get_cors_headers(),
-            'body': json.dumps({'message': 'Event added successfully'})
+            'body': json.dumps({'message': 'Event created successfully', 'eventID': event_id})
         }
     except ClientError as e:
         return {
             'statusCode': 500,
-            'headers': get_cors_headers(),
-            'body': json.dumps({'error': str(e)})
+            'body': json.dumps({'message': 'Error creating event', 'error': str(e)})
         }
-
-
-def handle_client_error(e):
-    error_code = e.response['Error']['Code']
-    if error_code == 'NoSuchKey':
-        return {
-            'statusCode': 200,
-            'headers': get_cors_headers(),
-            'body': json.dumps({'message': 'No events found for this month'})
-        }
-    else:
-        return {
-            'statusCode': 500,
-            'headers': get_cors_headers(),
-            'body': json.dumps({'error': str(e)})
-        }
-
-def get_cors_headers():
-    # Since the Lambda URL is handling CORS, we do not need to set these headers here.
-    return {
-        'Content-Type': 'application/json'
-    }
-
